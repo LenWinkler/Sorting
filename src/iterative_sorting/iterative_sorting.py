7d1c7799181c4229390b7939baae57a3536bb209
LenWinkler <lenwinkler86@gmail.com>
2020-02-18 02:50:55 -0500

    can't figure it out. done for the night

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -23,6 +23,32 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    
+    proceed = True # flag for while loop
+
+    while proceed == True: # keep looping while proceed is true
+        
+        print('looping', arr)
+        for i in range(0, len(arr)):
+
+            swap_occurred = False # var to send to while loop flag
+            
+            
+            if i < len(arr) - 1 and arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                print('swap before', swap_occurred)
+                swap_occurred = True
+                print('swap after', swap_occurred)
+                continue
+
+            elif i < len(arr) - 1 and arr[i] < arr[i + 1]:
+                print('continue')
+                continue
+            
+            elif i == len(arr) and swap_occurred == False:
+                print('len', len(arr) - 1)
+                print('swap in elif', swap_occurred)
+                proceed = False
 
     return arr
 
@@ -31,3 +57,7 @@ def bubble_sort( arr ):
 def count_sort( arr, maximum=-1 ):
 
     return arr
+
+    # original [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+
+    #          [1, 5, 4, 2, 8, 6, 0, 3, 7, 9]
